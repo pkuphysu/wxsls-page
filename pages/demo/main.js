2a38429dde52b010aca4abb670fa639112ed81da
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2021-04-07 22:41:40 +0800

    feat: ensureToken

--- pages/demo/main.js
+++ pages/demo/main.js
@@ -1,9 +1,10 @@
-import { getAuth } from '../../utils/wxauth'
+import { ensureToken, wxtoken } from '../../utils/wxauth'
 
 document.querySelector('#app').innerHTML = `
   <h1>Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `
 
-console.log(getAuth())
+console.log(ensureToken())
 console.log(window.env.API_URL)
+console.log(wxtoken)
--- utils/wxauth.js
+++ utils/wxauth.js
@@ -1,4 +1,31 @@
-export function getAuth () {
-  const token = localStorage.getItem('wxtoken')
-  return token
+export let wxtoken = localStorage.getItem('wxtoken')
+
+export function ensureToken () {
+  if (wxtoken !== null) return
+  const searchParams = new URLSearchParams(location.search)
+  const code = searchParams.get('code')
+  if (code === null) {
+    const redirectURL = new URL('https://open.weixin.qq.com/connect/oauth2/authorize')
+    const redirectParams = {
+      appid: window.env.APPID,
+      redirect_uri: location.href,
+      response_type: 'code',
+      scope: 'snsapi_base'
+    }
+    for (const paramKey in redirectParams) {
+      redirectURL.searchParams.append(paramKey, redirectParams[paramKey])
+    }
+    redirectURL.hash = 'wechat_redirect'
+    console.log('Not authed. Redirecting to', redirectURL.href)
+    location.replace(redirectURL.href)
+  } else {
+    const authURL = new URL('/auth', window.env.API_URL)
+    authURL.searchParams.append('code', code)
+    fetch(authURL.href)
+      .then(res => res.json())
+      .then(({ token }) => {
+        wxtoken = token
+        localStorage.setItem('wxtoken', token)
+      })
+  }
 }
