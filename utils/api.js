74cc3b08e6237b58a8e8828371a36e862e7253f4
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2021-04-17 15:56:48 +0800

    fix(api): Content-Type

--- utils/api.js
+++ utils/api.js
@@ -1,14 +1,16 @@
 export const requestApi = async (method, url, data) => {
   const fetchInit = {
+    headers: new Headers(),
     method,
     cache: 'no-cache',
     body: JSON.stringify(data)
   }
   const token = sessionStorage.getItem('token')
   if (token) {
-    fetchInit.headers = new Headers({
-      Authorization: 'Basic ' + token
-    })
+    fetchInit.headers.append('Authorization', 'Basic ' + token)
+  }
+  if (data) {
+    fetchInit.headers.append('Content-Type', 'application/json')
   }
   return fetch(new URL(url, window.env.API_URL), fetchInit)
     .then(resp => resp.json())
