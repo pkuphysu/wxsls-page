b61e0e194c6a0220180414960c4c2a00130d76a6
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2021-04-23 20:37:04 +0800

    feat: admin db page

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,8 +7,21 @@ module.exports = {
     'standard',
     'plugin:vue/recommended'
   ],
+  plugins: [
+    'svelte3'
+  ],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module'
+  },
+  overrides: [
+    {
+      files: ['*.svelte'],
+      processor: 'svelte3/svelte3'
+    }
+  ],
+  rules: {
+    // sveltejs/eslint-plugin-svelte3#82
+    'no-multiple-empty-lines': ['error', { max: 1, maxBOF: 2 }]
   }
 }
